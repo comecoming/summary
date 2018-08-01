6a009c8f656a7620fae67a4c50f33885df86fe8e
root <root@localhost.localdomain>
2018-08-01 00:04:41 -0700

    freetype update

--- sqlite/main.c
+++ sqlite/main.c
@@ -5,7 +5,7 @@
 static int callback(void *NotUsed, int argc, char **argv, char **azColName){
    int i;
 	char *str = NotUsed;
-	printf("callback %s:%d\n", str, argc);
+	printf("callback [%s]:%dcol\n", str, argc);
    for(i=0; i<argc; i++){
       printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
    }
@@ -26,17 +26,20 @@ int main(int argc, char* argv[])
 	if (sqlite3_open(argv[1], &db))
 		printf("%s", sqlite3_errmsg(db));
 
-	if (sqlite3_exec(db, "create table if not exists mytbl (id int, name text, pwr int, agi int, magic int)", callback, "now i create table", &errmsg))
+	if (sqlite3_exec(db, "create table if not exists mytbl (id int, name text, pwr int, agi int, magic int)", callback, "create mytbl", &errmsg))
 		printf("%s", sqlite3_errmsg(db));
 
-	if (sqlite3_exec(db, "insert into mytbl values (2, 'monkeyD', 100, 100, 30)", callback, "now i insert table", &errmsg))
-		printf("%s", sqlite3_errmsg(db));
+	//if (sqlite3_exec(db, "insert into mytbl values (2, 'monkeyD', 100, 100, 30)", callback, "now i insert table", &errmsg))
+	//	printf("%s", sqlite3_errmsg(db));
 
-	if (SQLITE_OK != sqlite3_exec(db, "select * from sqlite_master", callback, "select sqlite_master", &errmsg))
-		printf("%s", sqlite3_errmsg(db));
+	//if (SQLITE_OK != sqlite3_exec(db, "select * from sqlite_master", callback, "select sqlite_master", &errmsg))
+	//	printf("%s", sqlite3_errmsg(db));
 
 	if (SQLITE_OK != sqlite3_exec(db, "select * from mytbl where id=2", callback, "select mytbl", &errmsg))
 		printf("%s", sqlite3_errmsg(db));
+
+	//if (SQLITE_OK != sqlite3_exec(db, "delete from mytbl where id=2", callback, "select mytbl", &errmsg))
+	//	printf("%s", sqlite3_errmsg(db));
 
 	sqlite3_close(db);
 	return 0;
