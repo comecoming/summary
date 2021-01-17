37575942d465dafe0541b929a35b373a6eaf4ded
huangshanqin <huangshanqin@countrygarden.com.cn>
2021-01-17 21:50:38 +0800

    update stm32

--- libxml/test.c
+++ libxml/test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>  
 #include <string.h>  
 #include <stdlib.h>  
-#include <libxml/parser.h>  
+#include <libxml/parser.h>
   
 static int  
 parseDoc(char* docname) {  
@@ -25,19 +25,20 @@ parseDoc(char* docname) {
         return 0;  
     }  
   
-    //if(xmlStrcmp(cur->name, (const xmlChar*)"root")) {  
-    //    printf("end\n");  
-    //    return 0;  
-    //}  
+    if(xmlStrcmp(cur->name, (const xmlChar*)"root")) {  
+        printf("end\n");  
+        return 0;  
+    }  
   
+	printf("name:%s\n", cur->name);
+
     cur = cur->children;  
     while(cur != NULL) {  
 	printf("name:%s\n", cur->name);
 	cur2 = cur->children;
 	while (cur2 != NULL) {
-        	printf("name=%s content=%s\n",cur2->name,   
-        	        (char*)xmlNodeGetContent(cur2));  
-        	        //cur->content);  
+        	printf("name=%s content=%s\n",cur2->name, (char*)xmlNodeGetContent(cur2));  
+        	printf("name=%s content=%s\n",cur2->name, cur->content);  
         	cur2 = cur2->next;  
 	}
 	cur = cur->next;
